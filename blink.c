cc9331e78de2152fa589fcdad40bc7e8bf4c5599
Victor Lamoine <victor.lamoine@gmail.com>
2019-04-04 20:06:38 +0200

    Update CMake files

--- blink.c
+++ blink.c
@@ -1,7 +1,8 @@
 #include <FreeRTOS.h>
+#include <task.h>
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <task.h>
 
 static void
 task_blink(void *args __attribute((unused)))
