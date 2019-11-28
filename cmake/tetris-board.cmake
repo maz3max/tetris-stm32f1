56f052aea6898153ce475191999039c8154054ee
Maximilian Deubel <maximilian.deubel@ovgu.de>
2019-11-28 20:11:19 +0100

    correct specs: use newlib-nano

--- cmake/tetris-board.cmake
+++ cmake/tetris-board.cmake
@@ -1,6 +1,6 @@
 # STM32F0 Tetris-Board flags
 add_definitions(-DSTM32F0)
-set(STM32F0_FLAGS "-Os -g -mcpu=cortex-m0 -mthumb -msoft-float -MD -specs=nano.specs -specs=nosys.specs")
+set(STM32F0_FLAGS "-Os -g -mcpu=cortex-m0 -mthumb -msoft-float -MD -specs=nano.specs")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${STM32F0_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${STM32F0_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --static -nostartfiles -MD -T '${CMAKE_SOURCE_DIR}/libopencm3/lib/stm32/f0/stm32f03xz6.ld'")
