bd236905687d791060194867bffc36af81f9f0aa
Maximilian Deubel <maximilian.deubel@ovgu.de>
2019-12-01 02:25:03 +0100

    add cpu-specific def

--- cmake/tetris-board.cmake
+++ cmake/tetris-board.cmake
@@ -1,5 +1,5 @@
 # STM32F0 Tetris-Board flags
-add_definitions(-DSTM32F0)
+add_definitions(-DSTM32F0 -DSTM32F030R8C6)
 set(STM32F0_FLAGS "-Os -g -mcpu=cortex-m0 -mthumb -msoft-float -MD -specs=nano.specs")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${STM32F0_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${STM32F0_FLAGS}")
